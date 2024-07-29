28d16c1567a0561e3f5fbc9bc60904921d70f647
SamGiKim <rlawns256@naver.com>
2024-07-29 17:15:28 +0900

    update add기능 완성
    update add기능 완성

--- mybatisapp/src/main/java/com/studymavenspringboot/mybatisapp/category/CatWebController.java
+++ mybatisapp/src/main/java/com/studymavenspringboot/mybatisapp/category/CatWebController.java
@@ -32,29 +32,34 @@ public class CatWebController {
             List<ICategory> allList = this.categoryService.findAllByNameContains(searchCategoryDto);
             model.addAttribute("itemList", allList);
             model.addAttribute("searchCategoryDto", searchCategoryDto);
-            // java 에서 html 문자를 만드는 고전적인 방법은 매우 안 좋다.
-            String sPages = this.getHtmlPageString(searchCategoryDto);
-            model.addAttribute("pageHtml", sPages);
         } catch (Exception ex) {
             log.error(ex.toString());
             model.addAttribute("error_message", "오류가 발생했습니다. 관리자에게 문의하세요.");
-            return "error/error_save";  // resources/templates 폴더안의 화면파일
+            return "error/error_save";
         }
-        return "catweb/category_list";  // resources/templates 폴더안의 화면파일
+        return "catweb/category_list";
     }
 
-    private String getHtmlPageString(SearchCategoryDto searchCategoryDto) {
-        StringBuilder sResult = new StringBuilder();
-        int tPage = (searchCategoryDto.getTotal() + 9) / 10;
-        sResult.append("<div>");
-        for ( int i = 0; i < tPage; i++ ) {
-            sResult.append(" <a href='category_list?page=" + (i+1) +
-                    "&name=" + searchCategoryDto.getName() + "'>");
-            sResult.append(i+1);
-            sResult.append("</a> ");
+    @GetMapping("/category_add")
+    public String categoryAdd(Model model) {
+        model.addAttribute("categoryDto", new CategoryDto());
+        // 카테고리 추가화면 템플릿엔진에서 출력
+        return "catweb/category_add";
+    }
+    @PostMapping("/category_insert")
+    public String categoryInsert(@ModelAttribute CategoryDto dto, Model model) {
+        try {
+            if ( dto == null || dto.getName() == null || dto.getName().isEmpty() ) {
+                model.addAttribute("error_message", "이름이 비었습니다.");
+                return "error/error_bad";  // resources/templates 폴더안의 화면파일
+            }
+            this.categoryService.insert(dto);
+        } catch (Exception ex) {
+            log.error(ex.toString());
+            model.addAttribute("error_message", dto.getName() + " 중복입니다.");
+            return "error/error_save";
         }
-        sResult.append("</div>");
-        return sResult.toString();
+        return "redirect:category_list?page=1&name=";
     }
 
     @PostMapping("/catweb_update")
@@ -78,26 +83,6 @@ public class CatWebController {
         return "redirect:category_list?page=1&name=";
     }
 
-    @GetMapping("/catweb_insert")
-    public String catwebInsert(Model model, @RequestParam Long id) {
-        try {
-            if ( id == null || id <= 0 ) {
-                model.addAttribute("error_message", "ID는 1보다 커야 합니다.");
-                return "error/error_bad";
-            }
-            ICategory find = this.categoryService.findById(id);
-            if ( find == null ) {
-                model.addAttribute("error_message", id + " 데이터가 없습니다.");
-                return "error/error_find";
-            }
-            model.addAttribute("categoryDto", find);
-        } catch (Exception ex) {
-            log.error(ex.toString());
-            model.addAttribute("error_message", "서버 에러입니다. 관리자에게 문의 하세요.");
-            return "error/error_save";  // resources/templates 폴더안의 화면파일
-        }
-        return "catweb/catweb_insert";
-    }
 
     @GetMapping("/catweb_delete")
     public String categoryDelete(@RequestParam Long id, Model model){
